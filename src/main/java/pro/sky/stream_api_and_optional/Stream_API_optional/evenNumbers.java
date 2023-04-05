51fc52a2a239b4bf671bb9936ac631f7661d4d36
Polyxen1a <maslowa@inbox.ru>
2023-04-05 09:22:07 +0300

    Stream API и optional

---
+++ src/main/java/pro/sky/stream_api_and_optional/Stream_API_optional/evenNumbers.java
@@ -0,0 +1,24 @@
+package pro.sky.stream_api_and_optional.Stream_API_optional;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+public class evenNumbers {
+    public static void findEvenNumbers(Integer[] sourceNumbers) {
+
+        List<Integer> evens = new ArrayList<Integer>();
+        Stream<Integer> integerStreamOf = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        for (Integer sourceNumber : sourceNumbers) {
+            if (sourceNumber % 2 == 0) {
+                evens.add(sourceNumber);
+            }
+/*            Predicate<Integer> fn;
+            fn = (n) -> (n % 2) == 0;
+ */
+        }
+                System.out.println("Чётные числа: " + evens);
+
+            }
+        }
